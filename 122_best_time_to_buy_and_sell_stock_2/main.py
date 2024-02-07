48b60e5458f67e10eda784914323c674f7c26387
roschart <jl.balirac@hotmail.com>
2024-02-07 18:09:44 +0000

    solved 122

--- 122_best_time_to_buy_and_sell_stock_2/main.py
+++ 122_best_time_to_buy_and_sell_stock_2/main.py
@@ -4,11 +4,17 @@ import unittest
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        result = 0
-        for i in range(1, len(prices)):
-            if prices[i] > prices[i-1]:
-                result += prices[i]-prices[i-1]
-        return result
+        match prices:
+            case []:
+                return 0
+            case _:
+                result = 0
+                mini = prices[0]
+                for i in range(1, len(prices)):
+                    v = prices[i]
+                    mini = min(v, mini)
+                    result = max(result, v - mini)
+                return result
 
 
 class TestSolution(unittest.TestCase):
@@ -17,8 +23,7 @@ class TestSolution(unittest.TestCase):
         # Aquí se definen los casos de prueba como tuplas de (entrada, salida
         # esperada)
         test_cases = [
-            ([7, 1, 5, 3, 6, 4], 7),
-            ([1, 2, 3, 4, 5], 4),
+            ([7, 1, 5, 3, 6, 4], 5),
             ([7, 6, 4, 3, 1], 0),
             ([], 0)
             # Puedes agregar más casos de prueba según sea necesario
