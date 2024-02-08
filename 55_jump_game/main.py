dcde32892f5e7e3850623b051814ddcbaff11120
roschart <jl.balirac@hotmail.com>
2024-02-08 08:19:22 +0000

    solved

---
+++ 55_jump_game/main.py
@@ -0,0 +1,42 @@
+import unittest
+from typing import List
+
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        i = 0
+        indexes = [i]
+        done = set()
+        while len(indexes) > 0:
+            i = indexes.pop()
+            done.add(i)
+            if i >= len(nums)-1:
+                return True
+            v = nums[i]
+            indexes += [x for x in range(i+1, i+v+1) if x not in done]
+        return False
+
+
+class TestSolution(unittest.TestCase):
+    def test_canJump(self):
+        test_cases = [
+            ([2, 0, 6, 9, 8, 4, 5, 0, 8, 9, 1, 2, 9, 6, 8, 8, 0, 6, 3, 1, 2, 2,
+              1, 2, 6, 5, 3, 1, 2, 2, 6, 4, 2, 4, 3, 0, 0, 0, 3, 8, 2, 4, 0, 1,
+              2, 0, 1, 4, 6, 5, 8, 0, 7, 9, 3, 4, 6, 6, 5, 8, 9, 3, 4, 3, 7, 0,
+              4, 9, 0, 9, 8, 4, 3, 0, 7, 7, 1, 9, 1, 9, 4, 9, 0, 1, 9, 5, 7, 7,
+              1, 5, 8, 2, 8, 2, 6, 8, 2, 2, 7, 5, 1, 7, 9, 6], False),
+            ([2, 3, 1, 1, 4], True),
+            ([3, 2, 1, 0, 4], False),
+            ([2, 0], True),
+            ([2, 5, 0, 0], True),
+            ([2, 1, 0, 0], False)
+        ]
+        s = Solution()
+        for nums, expected in test_cases:
+            with self.subTest(nums=nums, expected=expected):
+                self.assertEqual(s.canJump(nums), expected,
+                                 f"Failed for nums: {nums}")
+
+
+if __name__ == "__main__":
+    unittest.main()
